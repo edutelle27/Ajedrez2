3cc74cbe83bf3686663a2c2d7fbbdb1a3acc575f
eduardo <chessbenidorm@gmail.com>
2022-12-14 14:06:56 +0100

    nueva prueba primer paso

---
+++ src/Ajedrez2.java
@@ -0,0 +1,9 @@
+public class Ajedrez2 {
+        public static void main(String [] args){
+            System.out.println(" Proyecto Ajedrez nuevo");
+            System.out.println("Otro commit");
+            System.out.println("tercer commit");
+            System.out.println("cuarto commit");
+            System.out.println("Quinto commit");
+    }
+}
